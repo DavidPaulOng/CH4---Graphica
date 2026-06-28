cce789155792e18ac3e2fc45e4133aa3ede0c4f8
David Paul Ong <davidpaulong@gmail.com>
2026-06-29 07:56:50 +0800

    first head overwrite

---
+++ Graphica/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Graphica
+//
+//  Created by David Paul Ong on 29/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Graphica/GraphicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphicaApp.swift
+//  Graphica
+//
+//  Created by David Paul Ong on 29/06/26.
+//
+
+import SwiftUI
+
+@main
+struct GraphicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
