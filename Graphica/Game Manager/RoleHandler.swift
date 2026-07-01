4430f19356beadbd1547d01cc586f29e7182e404
Belvanatha Gargita <satwikananda8@gmail.com>
2026-07-01 10:39:18 +0800

    add player class and roles enum, add role assignment

--- Graphica/Game Manager/RoleHandler.swift
+++ Graphica/Game Manager/RoleHandler.swift
@@ -1,7 +1,23 @@
-//
-//  RoleHandler.swift
-//  Graphica
-//
-//  Created by Kadek Belvanatha Gargita Satwikananda on 01/07/26.
-//
+import Foundation
+import Combine
 
+class RoleHandler: ObservableObject {
+    @Published var players: [Player] = []
+    
+    func assignGameRoles() {
+        guard !players.isEmpty else { return }
+        
+        var pool: [playerRole] = [.forger]
+        
+        while pool.count < players.count {
+            pool.append(.thief)
+        }
+        
+        pool.shuffle()
+        
+        for i in 0..<players.count {
+            players[i].role = pool[i]
+            players[i].isEliminated = false
+        }
+    }
+}
