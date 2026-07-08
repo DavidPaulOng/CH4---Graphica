36e0a0d134ea12999719939adce32c444105e454
Aulia Nadhirah Yasmin Badrulkamal <aulianadhirahyb@gmail.com>
2026-07-08 21:26:58 +0800

    add canvasVoting component

--- Graphica/Game State Screen/VotingView.swift
+++ Graphica/Game State Screen/VotingView.swift
@@ -8,10 +8,23 @@
 import SwiftUI
 import PencilKit
 
+
+//again, if it does not matches the actual implemenetation feel free to change it
+struct PlayerVoteStatus {
+    let isDead: Bool
+    let isCurrentUser: Bool
+}
+
 struct VotingView: View {
     @Environment(GameManager.self) var gameManager
     @State var selectedPlayerCanvas: PKDrawing = PKDrawing()
     @State var selectedPlayerID: String = ""
+    @State var tempVoters: [String: PlayerVoteStatus] = [
+        "boss": PlayerVoteStatus(isDead: false, isCurrentUser: false),
+        "nerd": PlayerVoteStatus(isDead: false, isCurrentUser: true),
+        "appreciator" : PlayerVoteStatus(isDead:true, isCurrentUser: true)
+    ]
+    var tempName = "Barra"
     
     var body: some View {
         ZStack{
@@ -22,20 +35,8 @@ struct VotingView: View {
                 // make sure you put the timer logic here
                 TimerRoleButton(secondsLeft: 50, secondsMax : 100, isTimerActive: true)
                     .padding(.horizontal, 44)
-                ZStack(){
-                    PKCanvasRepresentation(
-                        drawing: $selectedPlayerCanvas,
-                        selectedColor: .constant(Color.black),
-                        isInteractionEnabled: false,
-                        showToolPicker: false)
-                    .border(Color.black)
-                    .padding(10)
-                    .frame(width: 340, height: 423)
-                    Image("frameCanvas").allowsHitTesting(false)
-                }
+                CanvasVote(selectedPlayerCanvas: $selectedPlayerCanvas, playerName : tempName, voters: $tempVoters)
                 VStack(spacing:16){
-                    
-                    
                     HStack(){
                         let sortedPlayerIDs = gameManager.canvasHandler.playerCanvases[gameManager.currentRound].keys.sorted()
                         ForEach(sortedPlayerIDs, id: \.self) { playerID in
