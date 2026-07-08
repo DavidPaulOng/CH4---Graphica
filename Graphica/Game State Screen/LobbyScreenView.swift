d215bcddc9958c50944e392343a429fae556a26e
Runi Cholid <iamruni@icloud.com>
2026-07-08 10:28:41 +0800

    Refined LobbyScreenView and Created SettingScreenView
    Added background to LobbyScreenView and created SettingScreenView page (not yet formatted)

--- Graphica/Game State Screen/LobbyScreenView.swift
+++ Graphica/Game State Screen/LobbyScreenView.swift
@@ -14,89 +14,113 @@ struct LobbyScreenView: View {
     
     var body: some View {
         NavigationStack{
-            VStack(spacing:24){
-                
-                Image("Logo")
+            ZStack(){
+                Image("Crowningbg")
                     .resizable()
-                    .frame(width: 220.07, height: 68.93)
-                    
+                    .scaledToFit()
+                    .offset(y:350)
                 
-                ZStack(){
+                VStack(spacing:24){
                     
-                    Rectangle()
-                        .fill(Color.gray)
-                        .frame(width: 246.44, height: 337.29)
-                        .rotationEffect(Angle(degrees: -3.92))
-                    
-                    Image("Appreciator")
+                    Image("Logo")
                         .resizable()
-                        .frame(width:176.16, height: 264.25)
-                        .rotationEffect(Angle(degrees: -3.92))
+                        .frame(width: 220.07, height: 68.93)
+                        
                     
+                    ZStack(){
+                        
+                        
+                        Image("profileAppreciator")
+                            .resizable()
+                            .frame(width:176.16, height: 264.25)
+                            .rotationEffect(Angle(degrees: -3.92))
+                        
+                        Image("Frame")
+                            .resizable()
+                            .frame(width: 225, height: 310)
+                            .rotationEffect(Angle(degrees: -3.92))
+                        
+                        
+                    }
                     
-                }
-                
-                ZStack(){
-                    if showJoinField{
-                        VStack{
-                            HStack(){
-                                
-                                TextField("Room Code", text: $roomCode)
-                                    .textFieldStyle(.roundedBorder)
-                                    .frame(height:44)
-                                    .keyboardType(.numberPad)
-                                    .onChange(of: roomCode) {
-                                        if roomCode.count > 4 {
-                                            roomCode = String(roomCode.prefix(4))
+                    ZStack(){
+                        if showJoinField{
+                            VStack{
+                                HStack(){
+                                    
+                                    TextField("Room Code", text: $roomCode)
+                                        .textFieldStyle(CustomInputStyle())
+                                        .frame(width: 240)
+                                        .keyboardType(.numberPad)
+                                        .onChange(of: roomCode) {
+                                            if roomCode.count > 4 {
+                                                roomCode = String(roomCode.prefix(4))
+                                            }
                                         }
+                                    
+                                    Button("JOIN"){
+                                        
                                     }
-                                
-                                Button("JOIN"){
+                                    .buttonStyle(CustomButtonStyle(style: .primary))
+                                    .disabled(roomCode.count<4)
+                                    .frame(width: 79)
                                     
                                 }
-                                .buttonStyle(CustomButtonStyle(style: .primary))
-                                .disabled(roomCode.count<4)
-                                .frame(width: 79)
+                                
+                                Button("GO BACK"){
+                                           showJoinField = false
+                                }
+                                .buttonStyle(CustomButtonStyle(style: .textOnly))
                                 
                             }
+                            .transition(.move(edge: .trailing))
                             
-                            Button("GO BACK"){
-                                       showJoinField = false
+                        }else{
+                            
+                            VStack(){
+                                Button("JOIN A CREW") {
+                                        showJoinField = true
+                                }
+                                    .buttonStyle(CustomButtonStyle(style: .primary))
+                                    .frame(width: 335)
+                                
+                                
+                                Button("START A CREW") { }
+                                    .buttonStyle(CustomButtonStyle(style: .secondary))
+                                    .frame(width: 335)
                             }
-                            .buttonStyle(CustomButtonStyle(style: .textOnly))
-                            
+                            .transition(.move(edge: .leading))
                         }
-                        .transition(.move(edge: .trailing))
                         
-                    }else{
-                        
-                        VStack(){
-                            Button("JOIN A CREW") {
-                                    showJoinField = true
-                            }
-                                .buttonStyle(CustomButtonStyle(style: .primary))
-                            
-                            
-                            Button("START A CREW") { }
-                                .buttonStyle(CustomButtonStyle(style: .secondary))
-                        }
-                        .transition(.move(edge: .leading))
                     }
+                    .clipped()
+                    .animation(.easeOut(duration: 0.2), value: showJoinField)
                     
+                    Spacer()
                 }
-                .clipped()
-                .animation(.easeOut(duration: 0.2), value: showJoinField)
-                
-                Spacer()
             }
-            .padding(24)
+            .background(
+                Image("Lobbybg")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+            )
+//            .padding(.horizontal,24)
             .toolbar{
                 ToolbarItem(placement: .navigationBarTrailing){
-                    Button {
-                        showSettings = true
-                    } label: {
-                        Image(systemName: "gearshape")
+                    
+                    NavigationLink{
+                        SettingScreenView()
+                    }label: {
+                        Label("Setting", systemImage: "gearshape")
                     }
+                    
+                    
+//                    Button {
+//                        showSettings = true
+//                    } label: {
+//                        Image(systemName: "gearshape")
+//                    }
                 }
             }
         }
---
+++ Graphica/Game State Screen/SettingScreenView.swift
@@ -0,0 +1,23 @@
+//
+//  SettingScreenView.swift
+//  Graphica
+//
+//  Created by ROONEY on 08/07/26.
+//
+
+import SwiftUI
+
+struct SettingScreenView: View {
+    var body: some View {
+        
+        ZStack(){
+            
+            Text("Settings Screen")
+            
+        }
+    }
+}
+
+#Preview {
+    SettingScreenView()
+}
